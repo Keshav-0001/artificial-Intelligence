4d1e7036745c91da79d9adb31b6c1f68e8a97b7a
Keshav Choudhari <96710654+Keshav-0001@users.noreply.github.com>
2023-12-07 09:33:58 +0530

    Add files via upload

---
+++ practical_2.py
@@ -0,0 +1,18 @@
+def bayes_theorem(p_e1,p_e2,p_a_e,p_a_e2):
+
+    p_a= p_e1*p_a_e1+p_e2*p_a_e2
+
+    p_e1_a=p_e1*p_a_e1/p_a
+    return p_e1_a
+
+p_e1=1/2
+
+p_e2=1/2
+
+p_a_e1=3/5
+
+p_a_e2=3/7
+
+result=bayes_theorem(p_e1,p_e2,p_a_e1,p_a_e2)
+
+print('P(E1|A) = %.3f%%' %(result))
